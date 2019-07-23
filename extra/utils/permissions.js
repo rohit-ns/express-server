28c7036999a4ab62c7545f476c625bb7413f9924
rohitkr80777 <rohit.kumar@successive.tech>
2019-07-23 18:18:30 +0530

    added new files permission and validation

---
+++ extra/utils/permissions.js
@@ -0,0 +1,29 @@
+let permissions={
+    'getUsers': {
+    all: ['head-trainer'],
+    read : ['trainee', 'trainer'],
+    write : ['trainer'],
+    delete: [],
+    }
+    }
+    function hasPermission(moduleName, role, permissionType) {
+        if (
+        permissions[moduleName]["all"].includes(role) ||
+        permissions[moduleName][permissionType].includes(role))
+        {
+        console.log("True");
+        return ;
+        } 
+        else 
+        {
+        console.log("False");
+        return;
+        }}
+     
+        hasPermission("getUsers","head-trainer","all")
+
+
+
+   
+   
+
