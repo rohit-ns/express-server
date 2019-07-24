8674a17496155436ad0581804bb69a7c2f03d3ba
rohitkr80777 <rohit.kumar@successive.tech>
2019-07-24 12:21:09 +0530

    REMOVED IF

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,24 +1,18 @@
-let permissions={
+let permissions = {
     'getUsers': {
-    all: ['head-trainer'],
-    read : ['trainee', 'trainer'],
-    write : ['trainer'],
-    delete: [],
+        all: ['head-trainer'],
+        read : ['trainee', 'trainer'],
+        write : ['trainer'],
+        delete: [],
     }
-    }
-    function hasPermission(moduleName, role, permissionType) {
-        if (
-        permissions[moduleName]["all"].includes(role) ||
-        permissions[moduleName][permissionType].includes(role))
-        {
-        console.log("True");
-        return ;
-        } 
-        else 
-        {
-        console.log("False");
-        return;
-        }}
+};
+function hasPermission(moduleName, role, permissionType) {
+        const isAllowed = permissions[moduleName]["all"].includes(role)
+                || permissions[moduleName][permissionType].includes(role);
+
+        return isAllowed ? console.log("True") : console.log("False");
+        
+}
      
         hasPermission("getUsers","head-trainer","all")
 
--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -2,33 +2,28 @@ let users =[
     {
     traineeEmail: 'trainee@successive.tech',
     reviewerEmail: 'reviewer1@successive.tech',
-    },{
+    },  {
     traineeEmail: '*trainee2@successive.tech',
     reviewerEmail: 'reviewer21@successive.tech'
     },
     {
     traineeEmail: 'trainee@successive22.tech',
     reviewerEmail: '%reviewer1@successive.tech'
-    }
-    ]
+    }]  
     
-    function validateEmail(email) 
-    {
+function validateEmail(email) 
+{
     let x=/^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@successive.tech$/
-    if (x.test(email)){
-    return true;
-    }
-    else{
-    return false;
-    }
-    } 
-    function validateUser(new_users) {
+    return x.test(email)
     
+} 
+function validateUser(new_users) {
     let validuser = [];
     let invaliduser = [];
-    new_users.forEach(function(user) {
-    const { traineeEmail: a, reviewerEmail:b } = user; //using destructuring assign value to a,b
-    if (validateEmail(a)) {
+
+ new_users.forEach(function(user)  {
+ const { traineeEmail: a, reviewerEmail:b } = user; //using destructuring assign value to a,b
+    if (validateEmail(a))  {
     validuser.push(a);
     } else {
     invaliduser.push(a);
