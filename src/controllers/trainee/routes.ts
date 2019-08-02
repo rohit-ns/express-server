5c105180fe1852370a8db0653f8cd7512322cbfb
rohitkr80777 <rohit.kumar@successive.tech>
2019-08-02 16:29:15 +0530

    added validation handler

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import validationHandler from '../../libs/validationHandler';
+import validation from './validation';
 import { traineeController } from './Controller';
 const traineeRouter = new Router();
 traineeRouter.route('/')
-.get(traineeController.get)
-.post(traineeController.create)
-.put(traineeController.update)
-.delete(traineeController.delete);
+.get(validationHandler(validation.get), traineeController.get)
+.post(validationHandler(validation.create), traineeController.create)
+.put(validationHandler(validation.update), traineeController.update)
+.delete(validationHandler(validation.delete), traineeController.delete);
 export default traineeRouter;
---
+++ src/controllers/trainee/validation.ts
@@ -0,0 +1,55 @@
+const validation = {
+    create: {
+        id: {
+            custom: function(value) {
+                console.log('Value', value);
+                throw { error: 'Error Occured', message: 'Message',
+           };
+       },
+        in: ['body'],
+        required: true,
+        string: true,
+    },
+        name: {
+            errorMessage: 'Name is required',
+            in: ['body'],
+            regex: '',
+            required: true,
+    } },
+    delete: {
+        id: {
+            errorMessage: 'Id is required',
+            in: ['params'],
+            required: true,
+        } },
+    get: {
+     skip: {
+            default: 0,
+            errorMessage: 'Skip is invalid',
+         },
+            in: ['query'],
+            number: true,
+            required: false,
+    limit: {
+           default: 10,
+           errorMessage: 'Limit is invalid',
+           in: ['query'],
+           number: true,
+           required: false,
+    } },
+    update: {
+        dataToUpdate: {
+            custom: function(dataToUpdate) {
+                 },
+                 in: ['body'],
+                 isObject: true,
+                 required: true,
+         },
+        id: {
+            in: ['body'],
+            required: true,
+            string: true,
+            },
+  },
+};
+export default validation;
