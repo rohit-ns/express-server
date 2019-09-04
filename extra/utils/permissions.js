2dd7f3a3e4e2a7a71ff3a60d70a9f3a65b81a257
rohitkr80777 <rohit.kumar@successive.tech>
2019-09-04 16:07:37 +0530

    changes in permission

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,12 +1,19 @@
 
-import {permissions} from '.././constants';
-export default function hasPermission(moduleName, role, permissionType) {
-        const isAllowed = permissions[moduleName]["all"].includes(role)
-                || permissions[moduleName][permissionType].includes(role);
-
-        return isAllowed ? console.log("True") : console.log("False");
-        
-}
+import { permissions  } from '../constants' ;
+export default function  hasPermission( moduleName, role, permissionType) {
+    
+    if (!permissions.hasOwnProperty(moduleName)) {
+        return false;
+    }
+    else if (!permissions[moduleName].hasOwnProperty(permissionType)) {
+        return false;
+    }
+    else if (!permissions[moduleName][permissionType].includes(role)) {
+        return false;
+    }
+    else {
+        return true;
+    }}
      
        
 
